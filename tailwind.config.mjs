d7dae2620d2f94054f72625bed48d82c0ce96b81
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2024-03-02 13:32:50 -0300

    Add color palette. Fix: favicon on light mode. Remove unnecessary icon

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'selector',
 	theme: {
-		extend: {},
-	},
-	plugins: [],
+		extend: {
+			colors: {
+				'carrot-orange': {
+					'50': '#fffaeb',
+					'100': '#fdf0c8',
+					'200': '#fbe08c',
+					'300': '#f9ca50',
+					'400': '#f7b428',
+					'500': '#ec900e',
+					'600': '#d56e0a',
+					'700': '#b14c0c',
+					'800': '#903c10',
+					'900': '#763211',
+					'950': '#441704',
+				}
+			}
+		},
+	}
 }
